4397f4fc7b83ca0f13263976f62871610809e539
Alejo <alejo@archlinux.com>
2026-02-18 16:17:05 -0500

    fix: add brightness keys and update install script

--- commands/install_dependencies.sh
+++ commands/install_dependencies.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-sudo pacman -S zsh firefox git kitty feh thunar tmux which nvim
+sudo pacman -S --noconfirm zsh firefox kitty feh thunar tmux which nvim dunst picom brightnessctl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
 
 git clone https://github.com/zsh-users/zsh-autosuggestions \
   ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -12,3 +15,14 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
 git clone --depth 1 https://github.com/marlonrichert/zsh-autocomplete.git \
   ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
 
+sudo pacman -S --noconfirm pipewire-pulse pavucontrol zoom
+
+sudo pacman -S --needed base-devel git
+if ! command -v yay > /dev/null; then
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si --noconfirm
+  cd .. && rm -rf yay
+fi
+
+yay -S --noconfirm brave-bin zoom
--- commands/link_conf.sh
+++ commands/link_conf.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 sudo ln -sf ~/dotfiles/.zshrc ~/.zshrc
+sudo ln -sf ~/dotfiles/.xprofile ~/.xprofile
 sudo ln -sf ~/dotfiles/custom /usr/share/X11/xkb/symbols/custom
 sudo ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+sudo ln -sf ~/dotfiles/i3.config ~/.config/i3/config
+sudo ln -sf ~/dotfiles/kitty.config ~/.config/kitty/kitty.config
+mkdir -p ~/.config/picom
+sudo ln -sf ~/dotfiles/picom.conf ~/.config/picom.conf
